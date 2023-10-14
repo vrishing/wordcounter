e2f048f5175ff792f9cb53d8a1b6560462e5c31e
unknown <vrishin.gattu@gmail.com>
2023-10-14 23:01:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  
+  const [count, setCount] = useState(0);
+
+  let change = (e) => {
+    wordcount(e.target.value);
+  };
+
+  function wordcount(str) {
+    let c = 0;
+    let str1 = str.split(" ");
+
+    for (let i = 0; i < str1.length; i++) {
+      if (str1.length === 0) {
+        setCount(0);
+      }
+      if (str1[i] !== "") {
+        c++;
+      }
+      setCount(c);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <textarea id="input" rows="5" cols="40" onChange={change}></textarea>
+      <p id="word-counter">Words: {count} </p>
+      </center>
     </div>
   );
 }
